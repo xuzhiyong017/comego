e62b30d9535942c45f4f1d5213549066eb33f945
xuzhiyong <xuzhiyong@dasheng.tv>
2017-03-20 17:04:37 +0800

    删除无用文件

--- library/fw/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="302d4c35-fece-486b-83a7-c231e89d028b", modulePackage="com.duowan.fw", sdkRoot="E:\\android-sdk",layoutInfoDir="G:\\MySelfProject\\comego\\library\\fw\\build\\intermediates\\data-binding-info\\release",exportClassListTo="G:\\MySelfProject\\comego\\library\\fw\\build\\intermediates\\data-binding-info\\release\\_generated.txt",isLibrary=true,minSdk=10,enableDebugLogs=false,printEncodedError=true)
-public class DataBindingInfo {}
